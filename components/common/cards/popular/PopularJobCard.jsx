638e926847b001c84ea4154781d5661355ec1bbf
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-07-07 11:04:45 -0700

    Update PopularJobCard.jsx

---
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -0,0 +1,13 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+
+import styles from "./popularjobcard.style";
+import { checkImageURL } from "../../../../utils";
+
+const PopularJobCard = ({ job, selectedJob, handleCardPress }) => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default PopularJobCard;
